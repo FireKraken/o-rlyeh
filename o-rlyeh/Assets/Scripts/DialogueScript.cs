b6ef37bc791035b54c2a95304d757c78616d9239
Ashwath George <ashwath.george@gmail.com>
2014-07-01 15:01:09 -0400

    Added preliminary GUI scene and script

---
+++ o-rlyeh/Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueScript : MonoBehaviour 
+{
+	private string npcDialogue = "Schmaltzy dialogue goes here...";
+
+	private bool showLog = false;
+	private Rect logRect;
+
+	// Use this for initialization
+	void Start () 
+	{
+		logRect = new Rect (20, 20, 350, Screen.height * 2 / 3);	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI ()
+	{
+		GUILayout.BeginArea (new Rect (Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.8f, Screen.height * 0.8f));
+		GUILayout.BeginVertical ();
+
+		if (showLog)
+			logRect = GUI.Window (0, logRect, LogWindowFunction, "Log");
+
+		GUILayout.Space (Screen.height * 2 / 3);
+
+		GUILayout.BeginHorizontal ();
+
+		GUILayout.BeginVertical ();
+		GUILayout.FlexibleSpace ();
+		GUILayout.EndVertical ();
+
+		GUILayout.Box (npcDialogue);
+
+		GUILayout.BeginVertical ();
+		GUILayout.FlexibleSpace ();
+		GUILayout.EndVertical ();
+
+		GUILayout.EndHorizontal ();
+
+		GUILayout.EndVertical ();
+		GUILayout.EndArea ();
+	}
+
+	void LogWindowFunction (int windowID)
+	{
+		GUI.DragWindow ();
+	}
+}
