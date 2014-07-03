4b62e8c94ffa298f4e3446ad52d6d074788d8786
acbui <acqbui@gmail.com>
2014-07-03 09:52:25 -0400

    Updated Scripts

--- o-rlyeh/Assets/Scripts/GameControls/DialogueTracker.cs
+++ o-rlyeh/Assets/Scripts/GameControls/DialogueTracker.cs
@@ -28,27 +28,32 @@ public class DialogueTracker : MonoBehaviour {
 		public string[] options; 	// the player's possible responses to the paragraph
 		public string[] responses; // NPC response to the option picked
 
-		public dialogueSegment (string par, int m, string opt0, string opt1, string opt2, int n, string resp0, string resp1, string resp2)
+		public dialogueSegment(string par, int m, string[] opts, int n, string[] resps)
 		{
 			paragraph = par; 
 			numOpts = m;
 			numResp = n;
+
 			if (m > 0)
 			{
-				options = new string[m];
-				options[0] = opt0;
-				options[1] = opt1;
-				options[2] = opt2; 
+				for (int i = 0; i < m; i++)
+				{
+					options = new string[m];
+					options[i] = opts[i]; 
+				}
 			}
 			else 
 			{
 				options = null; 
 			}
-			if (n > 0){
-				responses = new string[3]; 
-				responses[0] = resp0;
-				responses[1] = resp1;
-				responses[2] = resp2;
+
+			if (n > 0)
+			{
+				for (int i = 0; i < n; i++)
+				{
+					responses = new string[n];
+					responses[i] = resps[i]; 
+				}
 			}
 			else 
 			{
@@ -81,7 +86,7 @@ public class DialogueTracker : MonoBehaviour {
 
 	void Start () 
 	{
-
+		makeConversations ();
 
 
 	}
@@ -158,122 +163,174 @@ public class DialogueTracker : MonoBehaviour {
 	 * IN START, save all the dialogues
 	 * -------------------------------------------------------------------------------------------------------------------------- */
 	private void makeConversations()
-	{
-		// DAY 1 - CAPTAIN
+	{	
+		string[] opts;
+		string[] resps; 
 
-		//string par, int n, string opt0, string opt1, string opt2, string resp0, string resp1, string resp2
+		//struct constructor: string par, int m, string[] opts, int n, string[] resps
+
+		// ----- DAY 1 - CAPTAIN -----
+
+		// CPT (0-0)
+		opts = new string[3] {	"OFFICIAL ANSWER" + addLike, 
+								"CASUAL ANSWER" + addSusp, 
+								"..."}; 
+
 		cpt [0] [0] = new dialogueSegment ("Hey there.", 
-		                                  	3,	"OFFICIAL ANSWER" + addLike, 
-		                                   		"CASUAL ANSWER" + addSusp, 
-		                                   		"...",
-		                                  	0, null, null, null); 
+		                                  	3, opts,
+		                                  	0, null); 
+
+		// CPT (0-1)
+		opts = new string[3] {	"Everything's fine.", 
+								"FLATTER HIM" + addLike, 
+								"..."}; 
 
 		cpt [0] [1] = new dialogueSegment ("You seem a little different today. Is everything ok?", 
-		                                  	3,	"Everything's fine.", 
-		                                   		"FLATTER HIM" + addLike, 
-		                                   		"...",
-		                                  	0, null, null, null);
+		                                  	3, opts,
+		                                  	0, null);
+
+		// CPT (0-2)
+		opts = new string[3] {	"That's what I wanted to talk to you about. They're going crazy! We need to leave as soon as possible!" + addSusp, 
+								"Well, the readings are showing some concerning results, but nothing certain yet." + addLike, 
+								"All is well, Captain."}; 
+
+		resps = new string[3] {	"Uh, are you sure it's not just a misreading?",
+								"Is that so? Hmm, that is a concern... Please let me know if the readings change.",
+								"Good to hear, keep up the good work."};
 
 		cpt [0] [2] = new dialogueSegment ("Any changes in the readings?", 
-		                                  	3,	"That's what I wanted to talk to you about. They're going crazy! We need to leave as soon as possible!" + addSusp, 
-		                                 		"Well, the readings are showing some concerning results, but nothing certain yet.",
-		                                   		"All is well, Captain.", 
-		                                  	3,	"Uh, are you sure it's not just a misreading?",
-				                                "Is that so? Hmm, that is a concern... Please let me know if the readings change.",
-		                                   		"Good to hear, keep up the good work.");
+		                                  	3, opts,  
+		                                  	3, resps);
+
+		// CPT (0-3)
+		opts = new string[3] { 	"Of course, sir." + addLike,
+								"I have better things to do, but very well.",
+								"..." + addSusp }; 
 
 		cpt [0] [3] = new dialogueSegment ("Well, now that you're here I do actually have a task you could help me with. I know you're a researcher, not a technician, but I'd need you to go down to maintenance and twist a double of knobs. I want to avoid leaving my post now that we're getting so close to the anomaly, so I'd really appreciate your help. Will you do it?",
-		                                  	3, "Of course, sir." + addLike, 
-		                                  		"I have better things to do, but very well.",
-		                                  		"..." + addSusp,
-		                                  	0, null, null, null); 
+		                                  	3, opts,
+		                                  	0, null); 
 
+		// CPT (0-4)
 		cpt [0] [4] = new dialogueSegment ("You really do seem different today... Maybe I'm just tired.",
-		                                   0, null, null, null,
-		                                   0, null, null, null);
+		                                   0, null,
+		                                   0, null);
 
-		// DAY 1 - PUG
+		// ----- DAY 1 - PUG -----
+
+		// PUG (0-0)
+		opts = new string[3] { 	"Yes master, I wanted to bask in your allmighty glory to start the day." + addLP,
+								"Yes, glorious master.",
+								"..."}; 
+
 		pug [0] [0] = new dialogueSegment ("You return, minion.",
-		                                   	3,	"Yes master, I wanted to bask in your allmighty glory to start the day." + addLP,
-		                                   		"Yes, glorious master.",
-		                                   		"...",
-		                                  	0, null, null, null); 
+		                                   	3, opts,
+		                                  	0, null); 
+
+		// PUG (0-1)
+		opts = new string[2] { 	"Bask in Ammutsepug's glory" + addSusp + addLP,
+								"Ask Ammutsepug for a task."}; 
+
 		pug [0] [1] = new dialogueSegment (null, 
-		                                   	2,	"Bask in Ammutsepug's glory" + addSusp + addLP,
-		                                  		"Ask Ammutsepug for a task.",
-		                                   		null,
-		                                   	0, null, null, null);
+		                                   	2, opts,
+		                                   	0, null);
 
-		// THIS ONLY TRIGGERS IF THE PLAYER SELECTED OPTION 2 (ASK FOR A TASK)
+		// PUG (0-2) * THIS ONLY TRIGGERS IF THE PLAYER SELECTED OPTION 2 (ASK FOR A TASK)
+		opts = new string[3] { 	"Yes, Great Slobbery One, Devourer of Stars (and Treats). I will gladly do your bidding, you beautiful being." + addLP,
+								"Very well, I will do your bidding.",
+								"..."};  
+
 		pug [0] [2] = new dialogueSegment ("Ah, you come to me for advice. A wise decision. This machine, this is what is bringing you towards me, is it not? Find a way to sabotage the machine so that it can not move towards me anymore.",
-		                                   3,	"Yes, Great Slobbery One, Devourer of Stars (and Treats). I will gladly do your bidding, you beautiful being." + addLP,
-		                                   		"Very well, I will do your bidding.",
-		                                   		"...", 
-		                                   0, null, null, null);
+		                                   3, opts,		 
+		                                   0, null);
 
-		// DAY 1 - QUEST
+		// ----- DAY 1 - QUEST -----
+		opts = new string[3] { 	"Twist knobs",
+								"Sabotage",
+								"Do nothing"};  
+
 		quest [0] = new dialogueSegment ("What do you want to do?", 
-		                                 3, "Twist knobs",
-		                                 	"Sabotage",
-		                                 	"Do nothing", 
-		                                 0, null, null, null); 
+		                                 3, opts, 
+		                                 0, null); 
 
-		// DAY 2 - CAPTAIN
+		// ----- DAY 1 - CAPTAIN -----
 
-		// IF PLAYER DID TASK, SHOW THIS. OTHERWISE, SKIP cpt[1][0]
+		// CPT (1-0) * IF PLAYER DID TASK, SHOW THIS. OTHERWISE, SKIP cpt[1][0]
+		opts = new string[3] { 	"OFFICIAL RESPONSE" + addLike,
+								"SMUG RESPONSE",
+								"DEFER COMPLIMENT" + addLike};
+
 		cpt [1] [0] = new dialogueSegment ("Ah, there you are! Good job yesterday, things are working as they should.", 
-		                                	3,	"OFFICIAL RESPONSE" + addLike,
-		                                 		"SMUG RESPONSE",
-		                                 		"DEFER COMPLIMENT" + addLike, 
-		                                 	0, null, null, null); 
+		                                	3, opts, 
+		                                 	0, null); 
+
+		// CPT (1-1) * THIS HAPPENS REGARDLESS
+		opts = new string[3] {	"That's what I wanted to talk to you about. They're going crazy! We need to leave as soon as possible!" + addSusp, 
+								"Well, the readings are showing some concerning results, but nothing certain yet." + addLike, 
+								"All is well, Captain."}; 
+		
+		resps = new string[3] {	"Uh, are you sure it's not just a misreading?",
+								"Is that so? Hmm, that is a concern... Please let me know if the readings change.",
+								"Good to hear, keep up the good work."};
 
 		cpt [1] [1] = new dialogueSegment ("So, any changes in the readings today?", 
-		                                   3,	"That's what I wanted to talk to you about. They're going crazy! We need to leave as soon as possible!" + addSusp, 
-		                                   		"Well, the readings are showing some concerning results, but nothing certain yet.",
-		                                   		"All is well, Captain.", 
-		                                   3,	"Uh, are you sure it's not just a misreading?",
-		                                   		"Is that so? Hmm, that is a concern... Please let me know if the readings change.",
-		                                   		"Good to hear, keep up the good work.");
+		                                   3, opts, 
+		                                   3, resps);
 
-		cpt [1] [1] = new dialogueSegment ("(YOU NOTICE THAT THE CAPTAIN LOOKS TIRED)", 
-		                                   3,	"Sir, you look quite tired. Would you like me to get you a caffeinated beverage pouch?" + addLike, 
-		                                  		"Sir, you're looking a bit tired.",
-		                                   		"...", 
-		                                   3,	"That would be great! Thank you!",
-		                                   		"Yes, I'm feeling quite tired indeed... Get me a pouch of coffee, will you?",
-		                                   		"Get me a pouch of coffee, will you?");
+		// CPT (1-2)
+		opts = new string[3] {	"Sir, you look quite tired. Would you like me to get you a caffeinated beverage pouch?" + addLike, 
+								"Sir, you're looking a bit tired.", 
+								"..."}; 
+		
+		resps = new string[3] {	"That would be great! Thank you!",
+								"Yes, I'm feeling quite tired indeed... Get me a pouch of coffee, will you?",
+								"Get me a pouch of coffee, will you?"};
 
-		cpt [1] [2] = new dialogueSegment ("Oh and I like it with milk, no sugar.", 
-		                                	0, null, null, null,
-		                                	0, null, null, null);
+		cpt [1] [2] = new dialogueSegment ("(YOU NOTICE THAT THE CAPTAIN LOOKS TIRED)", 
+		                                   3, opts, 
+		                                   3, resps);
+		// CPT (1-3)
+		cpt [1] [3] = new dialogueSegment ("Oh and I like it with milk, no sugar.", 
+		                                	0, null,
+		                                   	0, null);
 
-		// DAY 2 - PUG 
+		// ----- DAY 2 - PUG -----
 
-		// IF DID CAPTAIN'S QUEST OR NOTHING
+		// PUG (1-0) * IF DID CAPTAIN'S QUEST OR NOTHING
+		opts = new string[3] {	"MAKE EXCUSES",
+								"GROVEL AND APOLOGIZE",
+								"..."}; 
+		
+		resps = new string[1] { "No matter. There is still time." };
+
 		pug [1] [0] = new dialogueSegment ("You defy me, minion? You dare?! The ship is still moving towards me!",
-		                                   3,	"MAKE EXCUSES",
-		                                   		"GROVEL AND APOLOGIZE",
-		                                   		"...",
-		                                   1, 	"No matter. There is still time.", null, null); 
+		                                   3, opts,  		
+		                                   1, resps); 
 
-		// IF DID PUG QUEST
+		// PUG (1-1) * IF DID PUG QUEST
+		opts = new string[3] {	"GRACIOUSLY ACCEPT" + addLP,
+								"RELUCTANTLY ACCEPT",
+								"Graciously accept and praise the pug lord." + addLP}; 
+
 		pug [1] [1] = new dialogueSegment ("Well done, my minion. The ship has slowed down!		",
-		                                   3,	"GRACIOUSLY ACCEPT" + addLP,
-		                                   		"RELUCTANTLY ACCEPT",
-		                                   		"Graciously accept and praise the pug lord." + addLP,
-		                                   0, null, null, null); 
+		                                   3, opts,
+		                                   0, null); 
+		// PUG (1-2)
+		opts = new string[3] { 	"Yes, Great Slobbery One, Devourer of Stars (and Treats). I will gladly do your bidding, you beautiful being." + addLP,
+								"Very well, I will do your bidding.",
+								"..."};  
 
 		pug [1] [2] = new dialogueSegment ("Ah, you come to me for advice. A wise decision. This machine, this is what is bringing you towards me, is it not? Find a way to sabotage the machine so that it can not move towards me anymore.",
-		                                   3,	"Yes, Great Slobbery One, Devourer of Stars (and Treats). I will gladly do your bidding, you beautiful being.",
-		                                   		"Very well, I will do your bidding.",
-		                                   		"...",
-		                                   0, null, null, null);
+		                                   3, opts,
+		                                   0, null);
 
-		// DAY 2 - QUEST
+		// ----- DAY 2 - QUEST -----
+		opts = new string[3] { 	"Get coffee with milk" + addLike,
+								"Get coffee with sugar",
+								"Poison coffee" + addLP};  
+
 		quest [1] = new dialogueSegment ("What do you want to do?", 
-		                                 3, "Get coffee with milk" + addLike,
-		                                 	"Get coffee with sugar",
-		                                 	"Poison coffee" + addLP, 
-		                                 0, null, null, null);
+		                                 3, opts, 
+		                                 0, null);
 	}
 }
