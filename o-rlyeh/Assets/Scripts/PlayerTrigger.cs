e878d4f8054b7d1e740b9e8e09bf469b7f5ce782
Ashwath George <ashwath.george@gmail.com>
2014-07-03 12:40:32 -0400

    More tweaks
    Removed Mathf.SmoothStep, added Vector3.Slerp.

--- o-rlyeh/Assets/Scripts/PlayerTrigger.cs
+++ o-rlyeh/Assets/Scripts/PlayerTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerTrigger : MonoBehaviour 
 {
-	public float duration = 0.05f;
+	public float duration = 1.0f;
 
 	public Camera mainCam;
 
@@ -23,54 +23,52 @@ public class PlayerTrigger : MonoBehaviour
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
-		Vector3 curPos = new Vector3 (0, 0, 0);
-		curPos.x = mainCam.transform.position.x;
-		curPos.y = mainCam.transform.position.y;
-		curPos.z = mainCam.transform.position.z;
+		Vector3 curPos = new Vector3 (mainCam.transform.position.x, mainCam.transform.position.y, mainCam.transform.position.z);
+
+		float t = (Time.time - startTime) / duration;
 
 		if (other.tag == "MessHall")
 		{
 			mainCam.orthographicSize = 1.25f;
-			mainCam.transform.position = new Vector3 (Mathf.SmoothStep (curPos.x, -0.575f, duration), Mathf.SmoothStep (curPos.y, 0.175f, duration), curPos.z);
+			mainCam.transform.position = Vector3.Slerp (curPos, new Vector3 (-0.575f, 0.175f, curPos.z), t);
 			Debug.Log ("Entered mess hall.");
 		}
 		else if (other.tag == "EngineRoom")
 		{
 			mainCam.orthographicSize = 1.25f;
-			mainCam.transform.position = new Vector3 (Mathf.SmoothStep (curPos.x, -0.465f, duration), Mathf.SmoothStep (curPos.y, -2.175f, duration), curPos.z);
+			mainCam.transform.position = Vector3.Slerp (curPos, new Vector3 (-0.465f, -2.175f, curPos.z), t);
 			Debug.Log ("Entered engine room.");
 		}
 		else if (other.tag == "CrewQuarters")
 		{
 			mainCam.orthographicSize = 1.25f;
-			mainCam.transform.position = new Vector3 (Mathf.SmoothStep (curPos.x, -5.0f, duration), Mathf.SmoothStep (curPos.y, 0.175f, duration), curPos.z);
+			mainCam.transform.position = Vector3.Slerp (curPos, new Vector3 (-5.0f, 0.175f, curPos.z), t);
 			Debug.Log ("Entered crew quarters.");
 		}
 		else if (other.tag == "ObservationDeck")
 		{
 			mainCam.orthographicSize = 1.25f;
-			mainCam.transform.position = new Vector3 (Mathf.SmoothStep (curPos.x, 0.0f, duration), Mathf.SmoothStep (curPos.y, 2.5f, duration), curPos.z);
+			mainCam.transform.position = Vector3.Slerp (curPos, new Vector3 (0.0f, 2.5f, curPos.z), t);
 			Debug.Log ("Entered observation deck.");
 		}
 		else if (other.tag == "Bridge")
 		{
 			mainCam.orthographicSize = 1.5f;
-			mainCam.transform.position = new Vector3 (Mathf.SmoothStep (curPos.x, 4.275f, duration), Mathf.SmoothStep (curPos.y, 0.1f, duration), curPos.z);
+			mainCam.transform.position = Vector3.Slerp (curPos, new Vector3 (4.275f, 0.1f, curPos.z), t);
 			Debug.Log ("Entered bridge.");
 		}
 	}
 
 	void OnTriggerExit2D (Collider2D other)
 	{
-		Vector3 curPos = new Vector3 (0, 0, 0);
-		curPos.x = mainCam.transform.position.x;
-		curPos.y = mainCam.transform.position.y;
-		curPos.z = mainCam.transform.position.z;
+		Vector3 curPos = new Vector3 (mainCam.transform.position.x, mainCam.transform.position.y, mainCam.transform.position.z);
+		
+		float t = (Time.time - startTime) / duration;
 
 		if (other.tag == "MessHall" || other.tag == "EngineRoom" || other.tag == "CrewQuarters" || other.tag == "ObservationDeck" || other.tag == "Bridge")
 		{
 			mainCam.orthographicSize = 5.0f;
-			mainCam.transform.position = new Vector3 (Mathf.SmoothStep (curPos.x, -0.575f, duration), Mathf.SmoothStep (curPos.y, 0.175f, duration), curPos.z);
+			mainCam.transform.position = Vector3.Slerp (curPos, new Vector3 (-0.575f, 0.175f, curPos.z), t);
 			Debug.Log ("Moving through the ship.");
 		}
 	}
