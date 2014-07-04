8962aae237d1835b6f6300f7473b45da7af160fd
acbui <acqbui@gmail.com>
2014-07-04 14:40:42 -0400

    Title leads to Intro

--- o-rlyeh/Assets/Scripts/TitleScript.cs
+++ o-rlyeh/Assets/Scripts/TitleScript.cs
@@ -11,7 +11,7 @@ public class TitleScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.anyKeyDown) {
-			Application.LoadLevel("Main");
+			Application.LoadLevel("Intro");
 		}
 	}
 
