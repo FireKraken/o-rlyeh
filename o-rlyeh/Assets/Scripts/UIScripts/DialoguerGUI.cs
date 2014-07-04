a3605933557fac68901c05f21e43136697c6bf32
Ashwath George <ashwath.george@gmail.com>
2014-07-03 23:38:16 -0400

    Added working GUI scene with DayOne conversation
    Also added Dialoguer plugin for the fourth time.

---
+++ o-rlyeh/Assets/Scripts/UIScripts/DialoguerGUI.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialoguerGUI : MonoBehaviour 
+{
+	// Used to show and hide GUI on dialogue start and end
+	private bool _showing;
+
+	// Used to store text data
+	private string _text;
+
+	// Used to store text from choice data
+	private string[] _choices;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Listeners for the following events
+		Dialoguer.events.onStarted += onStarted;
+		Dialoguer.events.onEnded += onEnded;
+		Dialoguer.events.onTextPhase += onTextPhase;
+	}
+	
+	void OnGUI ()
+	{
+		if (!_showing)
+			return;
+
+		GUILayout.Box (_text);
+
+		if (_choices == null)
+		{
+			if (GUILayout.Button ("Next"))
+			{
+				Dialoguer.ContinueDialogue ();
+			}
+		}
+		else
+		{
+			for (int i = 0; i < _choices.Length; i++)
+			{
+				if (GUILayout.Button ((i + 1) + ".\t" + _choices[i]))
+				{
+					Dialoguer.ContinueDialogue (i);
+				}
+			}
+		}
+	}
+
+	// Called when dialogue starts
+	private void onStarted ()
+	{
+		// Show GUI
+		_showing = true;
+	}
+
+	// Called when dialogue ends
+	private void onEnded ()
+	{
+		// Hide GUI
+		_showing = false;
+	}
+
+	// Called when dialoguer is serving new text data
+	private void onTextPhase (DialoguerTextData data)
+	{
+		// Store text data from current phase
+		_text = data.text;
+		_choices = data.choices;
+	}
+}
---
+++ o-rlyeh/Assets/Scripts/UIScripts/InitializeDialogue.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializeDialogue : MonoBehaviour 
+{
+	void Awake ()
+	{
+		Dialoguer.Initialize ();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI ()
+	{
+		if (GUILayout.Button ("Begin"))
+		{
+			Dialoguer.StartDialogue (0, dialoguerCallback);
+			this.enabled = false;
+		}
+	}
+
+	private void dialoguerCallback ()
+	{
+		this.enabled = true;
+	}
+}
